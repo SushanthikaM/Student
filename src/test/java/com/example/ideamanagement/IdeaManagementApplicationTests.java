7ea8cfcff3ed019ea0dff4295bedac2eea57f680
SushanthikaM <susha.mani006@gmail.com>
2023-01-20 15:50:27 +0530

    my first commit

---
+++ src/test/java/com/example/ideamanagement/IdeaManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ideamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
