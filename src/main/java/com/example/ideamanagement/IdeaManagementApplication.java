102a1d7c78b0476a58afe64e7317cd143e8129f4
SushanthikaM <susha.mani006@gmail.com>
2023-01-20 16:02:13 +0530

    sec commit

--- src/main/java/com/example/ideamanagement/IdeaManagementApplication.java
+++ src/main/java/com/example/ideamanagement/IdeaManagementApplication.java
@@ -1,14 +1,1 @@
-package com.example.ideamanagement;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class IdeaManagementApplication {
-
-    public static void main(String[] args) {
-
-        SpringApplication.run(IdeaManagementApplication.class, args);
-    }
-
-}
+Fun with git
